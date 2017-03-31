2392a340b1ed1dcd437f14b60e7872dc5e687edf
Lehmniscate <mchlhtn@gmail.com>
2017-03-31 01:23:57 -0700

    fixed canvas styling and scaling issues

--- src/tanks_app.js
+++ src/tanks_app.js
@@ -1,8 +1,9 @@
 import World from './world';
 
 window.addEventListener("load", () => {
+  let canvasContainer = document.getElementById("canvas-container");
   let canvas = document.getElementById("canvas");
-  canvas.width = canvas.offsetWidth;
-  canvas.height = canvas.offsetHeight;
+  canvas.width = Math.floor(canvasContainer.offsetWidth);
+  canvas.height = Math.floor(canvasContainer.offsetHeight);
   let world = new World(canvas);
 });
